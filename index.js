8f7a6e156dbbedd57ee61c60b7c7790832fb8361
forest-shadow <mamasay@yandex.ru>
2020-05-19 13:08:54 +0300

    Parse movies url & save to `.json`

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const got = require('got');
-const cheerio = require('cheerio');
+const fs = require('fs');
+const getMovie = require('./getMovie')
+
+const URLS = [
+  'https://www.imdb.com/title/tt0102926/',
+  'https://www.imdb.com/title/tt2267998/'
+];
+
+(async () => {
+  const moviePromises = URLS.map(url => getMovie(url));
+  const movies = await Promise.all(moviePromises);
+  console.log(JSON.stringify({ movies }));
+  fs.writeFileSync('./movies.json', JSON.stringify({ movies }), 'utf-8');
+})();
